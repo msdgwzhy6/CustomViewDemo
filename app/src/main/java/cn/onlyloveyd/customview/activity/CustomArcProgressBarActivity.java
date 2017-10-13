d3f6f13383a61460ab4dc763e6669d718a25cae4
onlyloveyd <onlyloveyd@gmail.com>
2017-10-13 16:32:45 +0800

    名称更新

--- app/src/main/java/cn/onlyloveyd/customview/activity/CustomArcProgressBarActivity.java
+++ app/src/main/java/cn/onlyloveyd/customview/activity/CustomArcProgressBarActivity.java
@@ -19,7 +19,7 @@ public class CustomArcProgressBarActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.activity_arc_progressbar);
         ButterKnife.bind(this);
 
         Explode explode = new Explode();
